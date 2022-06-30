65fcf0264b70b6766da2ccd5ee39c88d923d9ff2
jovancvl <coldvikinglyrics@gmail.com>
2022-06-30 16:13:13 +0200

    start

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import sqlite3
+import os
+import json
 
 def practica1():
-    con = sqlite3.connect('example.db')
+    con = sqlite3.connect('database.db')
     cur = con.cursor()
-    cur.execute("CREATE TABLE usuario (dni text, nombre text, altura real)")
-    cur.execute("INSERT INTO usuario VALUES ('X', 'isaac', '1.85')")
+    cur.execute("CREATE TABLE users (dni text, nombre text, altura real)")
+    cur.execute("INSERT INTO users VALUES ('X', 'isaac', '1.85')")
     con.commit()
     con.close()
+
+
+
+
+
+    os.remove("database.db")
 
 
 if __name__ == '__main__':
